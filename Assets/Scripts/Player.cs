3f1cec4cad2945c72c8685e72cac35c1b6b3f62b
조하랑 <wawa111027@gmail.com>
2023-11-13 21:43:37 +0900

    플레이어 추가 / Player 스크립트 추가
    에셋도 추가 하였습니다

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 MoveVector = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatMove();
+    }
+
+    void UpdatMove()
+    {
+
+    }
+
+    public void ProcessInput(Vector3 moveDirection)
+    {
+
+    }
+
+}
