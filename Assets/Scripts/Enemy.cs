c6560885d2b59797aa14e97acf3b3664d050bad0
조하랑 <wawa111027@gmail.com>
2023-11-17 21:48:50 +0900

    적 오브젝트 추가 / 적 스크립트 추가
    적 오브젝트와 스크립트를 제작 하였습니다.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum State : int
+    {
+        None = -1,  // 사용전
+        Ready = 0,  // 준비 완료
+        Appear,     // 등장
+        Battle,     // 전투중
+        Dead,       // 사망
+        Disappear,  // 퇴장
+    }
+
+    [SerializeField]
+    State CurrentState = State.None;
+
+    /// 최고 속도
+    const float MaxSpeed = 10.0f;
+
+    const float MaxSpeedTime = 0.5f;
+
+    [SerializeField]
+    Vector3 TargetPosition;
+
+    [SerializeField]
+    float CurrentSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected void UpdateSpeed()
+    {
+
+    }
+
+    void UpdateMove()
+    {
+
+    }
+
+    void Arrived()
+    {
+
+    }
+
+    public void Appear(Vector3 targetPos)
+    {
+        TargetPosition = targetPos;
+        CurrentSpeed = MaxSpeed;
+
+        CurrentState = State.Appear;
+    }
+
+    void Disappear(Vector3 targetPos)
+    {
+        TargetPosition = targetPos;
+        CurrentSpeed = 0;
+
+        CurrentState = State.Disappear;
+    }
+
+
+}
