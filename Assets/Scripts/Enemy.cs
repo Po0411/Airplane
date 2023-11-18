4483bf41ea0bfb92d2e41e67e33b8ea49477c000
조하랑 <wawa111027@gmail.com>
2023-11-18 22:38:10 +0900

    적 스크립트 변경 추가 / 등장, 퇴장 씬 제작 코드
    적 스크립트를 수정 하였습니다

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -28,31 +29,90 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float CurrentSpeed;
 
+    Vector3 CurrentVelocity;
+
+    float MoveStartTime = 0.0f;
+
+    float BruteStartTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Appear(new Vector3(7.0f, 0.0f, 0.0f));
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Disappear(new Vector3(-15.0f, 0.0f, 0.0f));
+        }
+        */
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Appear(new Vector3(7.0f, transform.position.y, transform.position.z));
+        }
+
+        switch (CurrentState)
+        {
+            case State.None:
+            case State.Ready:
+                break;
+            case State.Dead: 
+                break;
+            case State.Appear:
+            case State.Disappear:
+                UpdateSpeed();
+                UpdateMove();
+                break;
+            case State.Battle:
+                UpdateBattle();
+                break;
+
+        }
 
     }
 
-    protected void UpdateSpeed()
+    void UpdateSpeed()
     {
-
+        CurrentSpeed = Mathf.Lerp(CurrentSpeed, MaxSpeed, (Time.time - MoveStartTime) / MaxSpeedTime);
     }
 
     void UpdateMove()
     {
+         float distance = Vector3.Distance(TargetPosition, transform.position);
+        if(distance == 0)
+        {
+            Arrived();
+            return;
+        }
 
+        CurrentVelocity = (TargetPosition - transform.position).normalized * CurrentSpeed;
+
+        // 속도 = 거리 / 시간 이므로 시간 = 거리 / 속도
+        transform.position = Vector3.SmoothDamp(transform.position, TargetPosition, ref CurrentVelocity, distance / CurrentSpeed, MaxSpeed);
     }
 
     void Arrived()
     {
+        CurrentSpeed = 0.0f;
+
+        if (CurrentState == State.Appear)
+        {
+            CurrentState = State.Battle;
+            BruteStartTime = Time.time;
+        }
+        else // if (CurrentState == State.Disappear)
+        {
+            CurrentState = State.None;
+        }
 
     }
 
@@ -62,15 +122,24 @@ public class Enemy : MonoBehaviour
         CurrentSpeed = MaxSpeed;
 
         CurrentState = State.Appear;
+        MoveStartTime = Time.time;
     }
 
     void Disappear(Vector3 targetPos)
     {
         TargetPosition = targetPos;
-        CurrentSpeed = 0;
+        CurrentSpeed = 0.0f;
 
         CurrentState = State.Disappear;
+        MoveStartTime = Time.time;
     }
 
+    void UpdateBattle()
+    {
+        if(Time.time - BruteStartTime > 3.0f)
+        {
+            Disappear(new Vector3(-15.0f, transform.position.y, transform.position.z));
+        }
+    }
 
 }
