3d3f258a8fb7a3a351acda28ff516a72d85329ce
조하랑 <wawa111027@gmail.com>
2023-11-21 01:04:29 +0900

    총알 스크립트 추가 / 총알 오브젝트 제작
    총알 스크립트와 총알 오브젝트를 제작 하였습니다.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum OwnerSide : int
+{
+    Player = 0,
+    Enemy
+}
+
+
+public class Bullet : MonoBehaviour
+{
+
+    OwnerSide ownerSde = OwnerSide.Player;
+
+    [SerializeField]
+    Vector3 MoveDirection = Vector3.up;
+
+    [SerializeField]
+    float Speed = 0.0f;
+
+    bool NeedMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+        if (!NeedMove)
+        {
+            return;
+        }
+
+        Vector3 moveVector = MoveDirection.normalized *  Speed * Time.deltaTime;
+
+        transform.position += moveVector; 
+    }
+
+    public void Fire(OwnerSide FireOwner, Vector3 firePosition, Vector3 direction, float speed)
+    {
+        ownerSde = FireOwner;
+        transform.position = firePosition;
+        MoveDirection = direction;
+        Speed = speed;
+
+        NeedMove = true;
+    }
+}
